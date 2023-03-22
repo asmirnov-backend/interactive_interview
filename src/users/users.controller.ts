2ebe9665ed748c961b89c0ee02313f90387a8364
Andrew Smirnov <smirnov@brick.dev>
2023-03-22 19:15:20 +0300

    Update a littel

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { User } from './interfaces/user.interface';
 import { UsersService } from './users.service';
 
